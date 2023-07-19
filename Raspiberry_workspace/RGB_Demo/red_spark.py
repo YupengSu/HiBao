74e27e590c3cd5a10b19db19008d9a0c538517fb
YupengSu <138129868+YupengSu@users.noreply.github.com>
2023-07-20 04:23:51 +0800

    change README

--- Raspiberry_workspace/RGB_Demo/red_spark.py
+++ Raspiberry_workspace/RGB_Demo/red_spark.py
@@ -1,48 +1,48 @@
-import time
-import random
-from rpi_ws281x import PixelStrip, Color
-from include import *
+    import time
+    import random
+    from rpi_ws281x import PixelStrip, Color
+    from include import *
 
-# Create NeoPixel object with appropriate configuration.
-#创建对象
-strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    # Create NeoPixel object with appropriate configuration.
+    #创建对象
+    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
-# Intialize the library (must be called once before other functions).
-#初始化
-strip.begin()
-print("begin successful")
+    # Intialize the library (must be called once before other functions).
+    #初始化
+    strip.begin()
+    print("begin successful")
 
-#Toggle all LED light colors
-#改变所以灯颜色
-def change_color(color, wait_ms=20):
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
+    #Toggle all LED light colors
+    #改变所以灯颜色
+    def change_color(color, wait_ms=20):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, color)
+            strip.show()
+            time.sleep(wait_ms / 1000.0)
+            
+    # 呼吸灯
+    #利用setBrightness()设置灯的亮度 值在0到255之间 包括 0和255
+    def breathing(color):
+        strip.setBrightness(0)
+        change_color(color)
         strip.show()
-        time.sleep(wait_ms / 1000.0)
-         
-# 呼吸灯
-#利用setBrightness()设置灯的亮度 值在0到255之间 包括 0和255
-def breathing(color):
-    strip.setBrightness(0)
-    change_color(color)
-    strip.show()
-    brightness = 0
-    increment = 1
-    count = 0
-    while True:
-        brightness = brightness + increment
-        strip.setBrightness(brightness)
-        strip.show()
-        time.sleep(0.001)
-        if brightness <= 0 or brightness >= 255:
-            increment = -increment
-            count = count + 1
-        if count == 7:
-            change_color(Color(0,0,0))
-            break
+        brightness = 0
+        increment = 1
+        count = 0
+        while True:
+            brightness = brightness + increment
+            strip.setBrightness(brightness)
+            strip.show()
+            time.sleep(0.001)
+            if brightness <= 0 or brightness >= 255:
+                increment = -increment
+                count = count + 1
+            if count == 7:
+                change_color(Color(0,0,0))
+                break
+            
         
-    
-try:
-    breathing(RED)
-except:
-    pass
+    try:
+        breathing(RED)
+    except:
+        pass
